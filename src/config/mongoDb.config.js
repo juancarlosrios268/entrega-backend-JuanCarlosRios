0373f37837c5a0879a6e593bc408b2269abe93bc
juan carlos rios <juancarlosriosr@gmail.com>
2024-12-24 10:41:48 -0500

    entrega final backend

--- src/config/mongoDb.config.js
+++ src/config/mongoDb.config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoDB = async () =>{
     try {
-        mongoose.connect("mongodb://localhost:27017/")
+        mongoose.connect("mongodb://localhost:27017/proyectoBackend")
         //mongoose.connect("mongodb+srv://administrador:12345@cluster0.457nj.mongodb.net/")
         console.log("MongoDB conectado")
     } catch{
